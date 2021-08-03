2668ab5aef6bec318a498d094ca9165aa2dd5bca
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2021-08-04 01:48:30 +0800

    fixed register + login

--- app/src/main/java/project/apps/kapekat/Login.java
+++ app/src/main/java/project/apps/kapekat/Login.java
@@ -26,7 +26,8 @@ import org.androidannotations.annotations.ViewById;
 import org.w3c.dom.Text;
 
 import io.realm.Realm;
-
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 @EActivity(R.layout.activity_login)
 
 public class Login extends AppCompatActivity
@@ -51,7 +52,8 @@ public class Login extends AppCompatActivity
 
     @AfterViews
     public void init(){
-
+        Realm.init(getApplicationContext());
+        realm = Realm.getDefaultInstance();
         //Code for Signup (start)
         String text = "Don't have an account yet? Sign Up";
         SpannableString ss = new SpannableString(text);
@@ -82,6 +84,43 @@ public class Login extends AppCompatActivity
 //        Boolean check = preferences.getBoolean("rememberme", false);
 //        String uuid = preferences.getString("uuid", null);
 
+    }
+    @Click(R.id.btnLogin)
+    public void login(){
+
+        SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+        String un = username.getText().toString();
+        String pword = password.getText().toString();
+
+        User results = realm.where(User.class).equalTo("username", un).findFirst();
+        if (results != null) {
+            String check_un = results.getUsername();
+            String check_pw = results.getPassword();
+            String check_uuid = results.getUuid();
+
+            if ((check_un.equals(un)) && (check_pw.equals(pword))) {
+                if (cb.isChecked()) {
+                    SharedPreferences.Editor edit = preferences.edit();
+                    edit.putBoolean("rememberme", true);
+                    edit.putString("uuid", check_uuid);
+                    edit.apply();
+                } else {
+                    SharedPreferences.Editor edit = preferences.edit();
+                    edit.putBoolean("rememberme", false);
+                    edit.putString("uuid", check_uuid);
+                    edit.apply();
+                }
+                MainMenu_.intent(this).start();
+            }
+            else {
+                Toast toast = Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+        if (results==null) {
+            Toast toast = Toast.makeText(this, "No User Found", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
 //    @Click(R.id.btnLogin)

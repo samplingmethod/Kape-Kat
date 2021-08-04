c554f9a8ce9a1f9c20beb7222ac205a992badb93
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2021-08-04 22:20:14 +0800

    review screen

---
+++ app/src/main/java/project/apps/kapekat/Review.java
@@ -0,0 +1,22 @@
+package project.apps.kapekat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.Realm;
+
+@EActivity(R.layout.activity_review)
+
+public class Review extends AppCompatActivity {
+}
